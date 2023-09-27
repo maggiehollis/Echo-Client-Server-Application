effd8f4509f0c2ba8f05c6a869bc6be92be5caba
maggiehollis <mhollis@smith.edu>
2023-09-26 20:20:03 -0400

    Updated client and server

--- echo-client.py
+++ echo-client.py
@@ -1,18 +1,51 @@
-#!/usr/bin/env python3
-
+#importing pyhton socket
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
-MSG = "Hello, world"
+#the server's IP address
+HOST = "127.0.0.1"
+#port of the server we are sending data to
+PORT = 65432 
 
-print("client starting - connecting to server at IP", HOST, "and port", PORT)
+# created a socket with type SOCK_stream (uses TCP) and sets IPv4 for the internet address
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    print(f"connection established, sending message '{MSG}'")
-    s.sendall(bytes(MSG, 'utf-8'))
-    print("message sent, waiting for reply")
-    data = s.recv(1024)
-
-print(f"Received response: '{data!r}' [{len(data)} bytes]")
-print("client is done!")
+    s.connect((HOST, PORT)) #connect to the server
+    print("Connection Established")
+    print("Would you like to preform a mathematical OPERATION or FIND a word in a sentence")
+    #user input for requested service
+    response = input()
+    
+    #if they want to do a mathematical operation read in information
+    if response == "OPERATION":  
+        print("What type of operation? Enter +, -, /, *, %")
+        op = input()
+        
+        print("Enter two numbers")
+        x = input()
+        y = input()
+        
+        #encode message with OP prefix and inputed values
+        equation = "OP##" + op + "##" + x + "##" + y
+        s.sendall(bytes(equation, "utf-8")) #send message to the server
+        print("Calculation sent")
+        data = s.recv(1024) #recieve the answer from the server
+        print("Answer recieved")
+        print(x + op + y + " = " + data.decode("utf-8")) #print out decoded (from bytes) result
+    
+     #if they want to do find a word in a sentence read in information
+    elif response == "FIND":
+        print("What sentence would you like to use?")
+        sentence = input()
+        
+        print("n is the nth word in the sentence. Enter n")
+        n = input()
+        
+        #encode message with FI prefix and inputed values
+        s.sendall(bytes("FI##" + sentence + "##" + n, "utf-8")) #send message to server
+        print("Calculation sent")
+        data = s.recv(1024) #recieve the answer from the server
+        print("Answer recieved")
+        print("Word #" + n + " is " + data.decode("utf-8")) #print out decoded (from bytes) result
+        
+    else:
+        print("That is not a valid response")
+        
--- echo-server.py
+++ echo-server.py
@@ -1,23 +1,92 @@
-#!/usr/bin/env python3
-
+#importing pyhton socket
 import socket
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+#this is the standard ip address to use for a loopback interface
+HOST = "127.0.0.1"
+#this is non-privileged the port the server is listening on
+PORT = 65432
 
 print("server starting - listening for connections at IP", HOST, "and port", PORT)
+# created a socket with type SOCK_stream (uses TCP) and sets IPv4 for the internet address
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
+    s.bind((HOST, PORT)) #associates this socket with our predefined host and port
+    s.listen() #socket is listening for connections
+    conn, addr = s.accept() #new socket conn when a connection is created
     with conn:
         print(f"Connected established with {addr}")
-        while True:
+        while True: #reads data until there is no more
             data = conn.recv(1024)
-            if not data:
+            
+            if not data: #stops the loop when no more data
                 break
-            print(f"Received client message: '{data!r}' [{len(data)} bytes]")
-            print(f"echoing '{data!r}' back to client")
-            conn.sendall(data)
+            
+            print("Data recieved: " + data.decode("utf-8"))
+            
+            #decode the input data at the ## separating different parts of the data
+            data = data.split(bytes("##", "utf-8"))
+            
+            #data is for computing an operation
+            if data[0] == bytes("OP", "utf-8"):                
+                try: #convert some of the input data into numbers if possible
+                    x = float(data[2])
+                    y = float(data[3])
+                except:
+                    print("The number(s) you entered are not valid")
+                    conn.sendall(bytes("ERROR", "utf-8"))
+                    break
+                
+                #set initial output
+                output = 0
+                
+                #locate value of part of the input data and compute
+                if data[1] == bytes("+", "utf-8"):
+                    output = x + y
+                elif data[1] == bytes("-", "utf-8"):
+                    output = x - y
+                elif data[1] == bytes("/", "utf-8"):
+                    output = x / y
+                elif data[1] == bytes("*", "utf-8"): 
+                    output = x * y
+                elif data[1] == bytes("%", "utf-8"):
+                    output = x % y
+                else:
+                    print("Not a valid operation")
+                    conn.sendall(bytes("ERROR", "utf-8"))
+                    break
+                    
+                #return solution back to client
+                print("Solution of " + str(output) + " has been found")
+                print(f"Sending solution back")
+                conn.sendall(bytes(str(output), "utf-8"))
+                
+            #data is for finding a word in a sentence
+            elif data[0] == bytes("FI", "utf-8"):
+                #split the sentence at the end of the input data into an array of words
+                sentence = data[1].decode("utf-8").split(" ")
+                
+                try: #convert the requested nth word to a number if possible
+                    n = int(data[2]) - 1
+                except:
+                    print("n is not a valid integer")
+                    conn.sendall(bytes("ERROR", "utf-8"))
+                    break
+                
+                #check to make sure n is not out of bounds
+                if n >= len(sentence) or n < 0:
+                   print("n is not valid")
+                   conn.sendall(bytes("ERROR", "utf-8"))
+                   break
+            
+                #send back the word
+                print("Requested word is " + sentence[n])
+                print(f"Sending solution back")
+                conn.sendall(bytes(sentence[n], "utf-8"))
+                
+            else:
+                print("That is not a valid preformance.")
+                conn.sendall(bytes("ERROR", "utf-8"))
+                break 
+    
+            
 
-print("server is done!")
+print("Server is done!")
